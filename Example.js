aeb0d11b3a2e696e67ea147308a2d5b90df7a890
RandomGamerPerson111 <noperson.png@gmail.com>
2025-04-22 09:49:15 -0700

    Update Example.js

--- Example.js
+++ Example.js
@@ -1,13 +1,14 @@
 const abbreviationssimple = ["M", "B", "T", "Qa", "Qn", "Sx", "Sp", "Oc", "No"]
-const ComplexEarly = ["","U","D","T","Qa","Qn","Sx","Sp","O","N"]
-const ComplexLate = ["Dc", "Vg", "Tg", "Ug", "Qi", "Se", "Sg", "Og", "Ng", "Ce"]
+const Complex1 = ["","U","D","T","Qa","Qn","Sx","Sp","O","N"]
+const Complex2 = ["Dc", "Vg", "Tg", "Ug", "Qi", "Se", "Sg", "Og", "Ng",]
+const Complex3 = ["Ce", "Du", "Tr", "Qu", "Qt", "Sc", "St", "Ot", "Nn"]
 var abbreviations = []
 for (var item in abbreviationssimple) {
   abbreviations.push(abbreviationssimple[item])
 }
-for (var item2 in ComplexLate) {
-  for (var Early in ComplexEarly) {
-    abbreviations.push(ComplexEarly[Early]+ComplexLate[item2])
+for (var item2 in Complex2) {
+  for (var Early in Complex1) {
+    abbreviations.push(Complex1[Early]+Complex2[item2])
   }
 }
 for (var thingy in abbreviations) {
