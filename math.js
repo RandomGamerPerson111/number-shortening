6e22efafe9ce3a6a26bf846c627747a8838f8948
RandomGamerPerson111 <noperson.png@gmail.com>
2025-04-22 09:04:21 -0700

    Update math.js

--- math.js
+++ math.js
@@ -1,4 +1,4 @@
-abbreviations = ['K', 'M', 'B', 'T', 'Qa', 'Qi', 'Sx', 'Sp', 'Oc', 'No', 'Dc']
+abbreviations = ['K', 'M', 'B', 'T', 'Qa', 'Qn', 'Sx', 'Sp', 'Oc', 'No', 'Dc','UDc', 'DDc', 'TDc', 'QaDc', 'QnDc', 'SxDc', 'SpDc', 'ODc', 'NDc', 'Vg','UVg', 'DVg', 'TVg', 'QaVg', 'QnVg', 'SxVg', 'SpVg', 'OVg', 'NVg', 'Tg','UTg', 'DTg', 'TTg', 'QaTg', 'QnTg', 'SxTg', 'SpTg', 'OTg', 'NTg', 'Qg','UQg', 'DQg', 'TQg', 'QaQg', 'QnQg', 'SxQg', 'SpQg', 'OQg', 'NQg', 'Qi','UQi', 'DQi', 'TQi', 'QaQi', 'QnQi', 'SxQi', 'SpQi', 'OQi', 'NQi', 'Se','USe', 'DSe', 'TSe', 'QaSe', 'QnSe', 'SxSe', 'SpSe', 'OSe', 'NSe', 'Sg','USg', 'DSg', 'TSg', 'QaSg', 'QnSg', 'SxSg', 'SpSg', 'OSg', 'NSg', 'Og','UOg', 'DOg', 'TOg', 'QaOg', 'QnOg', 'SxOg', 'SpOg', 'OOg', 'NOg', 'Ng','UNg', 'DNg', 'TNg', 'QaNg', 'QnNg', 'SxNg', 'SpNg', 'ONg', 'NNg', 'Ce']
 
 
 
@@ -8,19 +8,19 @@ function formatnumber(num) {
     tier = (Math.floor(Math.log10(absnum)/3)-1)
     suffex = abbreviations[tier] || ''
     scaled = (num/(10**(3*tier+3)))
-    switch(Math.floor(Math.log10(scaled))) {
+    switch(Math.floor(Math.log10(Math.abs(scaled)))) {
         case 0: 
-            roundedscaled = (Math.floor(100*scaled))/100
+            roundedscaled = (Math.round(100*scaled))/100
             break;
         case 1:
-            roundedscaled = (Math.floor(10*scaled))/10
+            roundedscaled = (Math.round(10*scaled))/10
             break;
         case 2:
-            roundedscaled = Math.floor(scaled)
+            roundedscaled = Math.round(scaled)
             break;
         default:
             console.log('switch is broken')
-            console.log(Math.floor(Math.log10(scaled)))
+            console.log(Math.round(Math.log10(scaled)))
             console.log(scaled)
     }
     return roundedscaled+suffex
