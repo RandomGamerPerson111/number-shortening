f7973c4122bf487ea933d68f9d4e7883fccc69c8
RandomGamerPerson111 <noperson.png@gmail.com>
2025-04-22 10:28:35 -0700

    Update math-with-an-e?.js

--- math-with-an-e?.js
+++ math-with-an-e?.js
@@ -1,11 +1,16 @@
 function abbrev(num) {
-  const abbreviationssimple = ["K", "M", "B", "T", "Qa", "Qn", "Sx", "Sp", "Oc", "No"]
+  const abbreviationssimple = ["", "K", "M", "B", "T", "Qa", "Qn", "Sx", "Sp", "Oc", "No"]
   const Complex1 = ["","U","D","T","Qa","Qn","Sx","Sp","O","N"]
-  const Complex2 = ["Dc", "Vg", "Tg", "Ug", "Qi", "Se", "Sg", "Og", "Ng",]
-  const Complex3 = ["Ce", "Du", "Tr", "Qu", "Qt", "Sc", "St", "Ot", "Nn"]
+  const Complex2 = ["", "Dc", "Vg", "Tg", "Ug", "Qi", "Se", "Sg", "Ot", "Nn",]
+  const Complex3 = ["","Ce", "Du", "Tr", "Qu", "Qt", "Sc", "St", "Og", "Ng"]
   if (num < 10) {return(abbreviationssimple[num])}
   else { 
-    
+    const var1 = num%10
+    const var2 = (Math.floor((num%100)/10))
+    const var3 = (Math.floor((num%1000)/100))
+    const output = ((Complex1[var1])+(Complex2[var2])+(Complex3[var3]))
+    return output
   }
 
 }
+
