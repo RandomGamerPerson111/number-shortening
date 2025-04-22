71fe6e4f6a614b373072150f4177e5d4c63264a4
RandomGamerPerson111 <noperson.png@gmail.com>
2025-04-22 10:09:47 -0700

    Update math-with-an-e?.js

--- math-with-an-e?.js
+++ math-with-an-e?.js
@@ -1,1 +1,11 @@
+function abbrev(num) {
+  const abbreviationssimple = ["K", "M", "B", "T", "Qa", "Qn", "Sx", "Sp", "Oc", "No"]
+  const Complex1 = ["","U","D","T","Qa","Qn","Sx","Sp","O","N"]
+  const Complex2 = ["Dc", "Vg", "Tg", "Ug", "Qi", "Se", "Sg", "Og", "Ng",]
+  const Complex3 = ["Ce", "Du", "Tr", "Qu", "Qt", "Sc", "St", "Ot", "Nn"]
+  if (num < 10) {return(abbreviationssimple[num])}
+  else { 
+    
+  }
 
+}
